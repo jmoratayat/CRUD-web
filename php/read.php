3214c0f6b31967dd09e771fb9c6d7d94accd0bb1
jmoratayat <jmoratayat2@miumg.edu.gt>
2021-08-30 22:40:00 -0600

    CSS

--- php/read.php
+++ php/read.php
@@ -1,14 +1,30 @@
 <head>
     <title>Ver datos</title>
-  <script type="module" src="../components/bmenu.js"></script>
+    <script type="module" src="../components/bmenu.js"></script>
     <barra-menu></barra-menu>
 </head>
-
 <?php
 require_once '../php/conexion.php';
 $sql = "SELECT * FROM parcial";
 $result = mysqli_query($conn, $sql);
 echo "
+        <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+        
+        td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+        
+        tr:nth-child(even) {
+            background-color: #dddddd;
+        }
+        </style>
         <table id='example' class='table table-responsive table-striped table-bordered table-condensed table-hover'>
         <tr>
                 <th>Codigo</th>
@@ -28,5 +44,4 @@ while ($row = mysqli_fetch_array($result)) {
             </tr>";
 }
 echo "</table>";
-
 ?>
